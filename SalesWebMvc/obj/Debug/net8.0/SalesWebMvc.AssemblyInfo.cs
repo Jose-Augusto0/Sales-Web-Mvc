24ff6548b42ef7c1662ba07ea2e458a70b6181b6
José Augusto <joseaugustogomessantos12@gmail.com>
2024-09-26 19:53:23 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fb63e2aa8725a45393731c7810c4bb6f0d0078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c801bd63b2bda2109f374292217b55e012ee5895")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
