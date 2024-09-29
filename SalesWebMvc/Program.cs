36e0f3c64d9d0a5ae75e8afe234a89b9f1cf1375
José Augusto <joseaugustogomessantos12@gmail.com>
2024-09-29 13:44:17 -0300

    Seeding service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,15 +1,29 @@
-﻿using System.Configuration;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-   options.UseMySql(
-       builder.Configuration.GetConnectionString("SalesWebMvcContext"),
-       new MySqlServerVersion(new Version(8, 0, 39)), 
-       mySqlOptions => mySqlOptions.MigrationsAssembly("SalesWebMvc") 
-   ));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+        new MySqlServerVersion(new Version(8, 0, 39)),
+        mySqlOptions => mySqlOptions.MigrationsAssembly("SalesWebMvc")
+    ));
+
+// Adicionando o SeedingService
+builder.Services.AddScoped<SeedingService>();
+
+// Configuração de internacionalização (opcional, mas de acordo com seu exemplo)
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,10 +31,22 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.UseRequestLocalization(localizationOptions); // Ativando a cultura
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    // Rodar o serviço de seeding (para popular o banco de dados em ambiente de desenvolvimento)
+    using (var scope = app.Services.CreateScope())
+    {
+        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+        seedingService.Seed(); // Chama o método de seed
+    }
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d4591c2b81944b2d587789e4920e4c55ddf2c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fcb102ed43bca39f05d649af531585d9443cd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
