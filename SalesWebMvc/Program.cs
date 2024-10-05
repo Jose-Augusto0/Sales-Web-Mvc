a6ce4904349ff84aea723938dbdaa391e9f4d3d4
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-05 13:20:49 -0300

    sellerFormViewModel

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 
 
--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ef80b71de62847982d65492a5101cb4a1f4bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393efa3f426170737ebc3ece4c3c13c9a9c61025")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
