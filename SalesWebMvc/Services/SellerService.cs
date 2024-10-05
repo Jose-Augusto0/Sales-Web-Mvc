6425a1a1941ced4e9c8b379787b0c7e293205797
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-05 14:39:28 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,24 +4,37 @@ using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Services
 {
-    public class SellerService
-    {
-        private readonly SalesWebMvcContext _context;
+	public class SellerService
+	{
+		private readonly SalesWebMvcContext _context;
 
-        public SellerService(SalesWebMvcContext context)
-        {
-            _context = context;
-        }
+		public SellerService(SalesWebMvcContext context)
+		{
+			_context = context;
+		}
 
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.ToList();
-        }
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.ToList();
+		}
 
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
-    }
+		public void Insert(Seller obj)
+		{
+			_context.Add(obj);
+			_context.SaveChanges();
+		}
+
+		public Seller FindbyId(int id)
+		{
+			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+		}
+
+		public void Remove(int id)
+		{
+			var obj = _context.Seller.Find(id);
+			_context.Seller.Remove(obj);
+			_context.SaveChanges();
+		}
+
+	}
 }
--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393efa3f426170737ebc3ece4c3c13c9a9c61025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ce4904349ff84aea723938dbdaa391e9f4d3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
