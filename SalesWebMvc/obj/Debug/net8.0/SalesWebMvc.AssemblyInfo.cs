e1d2ca6f2f2ce2cbb1b33c8d9859990209ca9dee
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-10 21:01:52 -0300

    Preparing sales search navigation views

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3319f322d2314c8c88b70da0327ab694df33ac75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4def54a1dbce1628e19864d549408ca19e1e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
