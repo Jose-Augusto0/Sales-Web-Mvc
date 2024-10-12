8e6dd64320ec78e1abec578536c4c141abe88f10
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-12 10:35:36 -0300

    criando novo servico

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<SalesRecord> FindByDate(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+        }
+    }
+}
--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4def54a1dbce1628e19864d549408ca19e1e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d2ca6f2f2ce2cbb1b33c8d9859990209ca9dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
